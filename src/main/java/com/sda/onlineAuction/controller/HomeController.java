77c89857d30b184d4131e3385fe36bd46c777d16
DESKTOP-BUQBTSL\Paulica <paul_qp@yahoo.com>
2021-08-09 22:00:16 +0300

    Implemented validator for productDto

--- src/main/java/com/sda/onlineAuction/controller/HomeController.java
+++ src/main/java/com/sda/onlineAuction/controller/HomeController.java
@@ -2,9 +2,11 @@ package com.sda.onlineAuction.controller;
 
 import com.sda.onlineAuction.dto.ProductDto;
 import com.sda.onlineAuction.service.ProductService;
+import com.sda.onlineAuction.validator.ProductDtoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,8 +15,11 @@ public class HomeController {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private ProductDtoValidator productDtoValidator;
+
     @GetMapping("/home")
-    public String getHomePage(){
+    public String getHomePage() {
         return "home";
     }
 
@@ -24,18 +29,21 @@ public class HomeController {
         //getAddItemPage - handler
         //model = parametru
         //dispecerul cauta un handler(vezi video ora 19)
-        // System.out.println("helooo");
+       System.out.println("Rulez get pe /addItem");
         ProductDto productDto = new ProductDto();
         model.addAttribute("productDto", productDto);
-        return "addItem";
+        return "addItem"; //pagina intoarsa din templates
     }
 
     @PostMapping("/addItem")
-    public String postAddItemPage(ProductDto productDto) {
+    public String postAddItemPage(Model model, ProductDto productDto, BindingResult bindingResult) {
         System.out.println("Am primit " + productDto);
-        productService.add(productDto);
-        return "addItem";
-
+        productDtoValidator.validate(productDto, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "addItem";
+        }
+            productService.add(productDto);
+        return "redirect:/addItem"; //rulez redirect catre get
     }
-
 }
+
